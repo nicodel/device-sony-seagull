75f5c0bf2e22b24b2546103de233ff147510cebe
nicodel <nicolas@nicodel.com>
2015-11-18 17:28:40 +0100

    Bug 1221152: Add Xperia T3 (seagull) B2G

--- aosp_d5103.mk
+++ aosp_d5103.mk
@@ -17,7 +17,7 @@ TARGET_KERNEL_CONFIG := aosp_yukon_seagull_defconfig
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/yukon/device.mk)
-$(call inherit-product, vendor/sony/seagull/seagull-vendor.mk)
+$(call inherit-product-if-exists, vendor/sony/seagull/seagull-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, prebuilts/chromium/webview_prebuilt.mk)
 $(call inherit-product-if-exists, vendor/google/products/gms.mk)
--- extract-files.sh
+++ extract-files.sh
@@ -1,4 +1,6 @@
-# Copyright 2014 The Android Open Source Project
+#!/bin/bash
+
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/seagull.mk
+DEVICE=seagull
+MANUFACTURER=sony
+STOCK=18.1.A.2.25
+
+. ../yukon/extract-files.sh
